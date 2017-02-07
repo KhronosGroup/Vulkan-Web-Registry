8dc32de03b586ea89b30c3ea98f8646a89baa55a
Jon Leech <oddhack@sonic.net>
2017-02-06 16:28:39 -0800

    Fix (R) mark

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include_once("../../assets/static_pages/khr_page_top.php");
 <h1 class="pagetitle">Khronos Vulkan&reg; Registry</h1>
 <div id="mainformat">
 
-<h1 style="text-align:center"> Khronos Vulkan&trade; Registry </h1>
+<h1 style="text-align:center"> Khronos Vulkan&reg; Registry </h1>
 
 <p> The Vulkan registry contains formatted specifications of the Vulkan API,
     header files, API reference pages, the reference card, and related
