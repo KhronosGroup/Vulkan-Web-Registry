f8c91a2689f47d12aaf714fdebce9abaa31d22c4
Jon Leech <oddhack@sonic.net>
2017-02-26 23:18:27 -0800

    Push Vulkan 1.0.42 spec update to the registry.

--- extensions.php
+++ extensions.php
@@ -1,11 +1,13 @@
 <ul>
     <li> <b>  KHR Extensions (Khronos)  </b> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHR_android_surface"> VK_KHR_android_surface </a> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHR_descriptor_update_template"> VK_KHR_descriptor_update_template </a> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHR_display"> VK_KHR_display </a> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHR_display_swapchain"> VK_KHR_display_swapchain </a> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHR_get_physical_device_properties2"> VK_KHR_get_physical_device_properties2 </a> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHR_maintenance1"> VK_KHR_maintenance1 </a> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHR_mir_surface"> VK_KHR_mir_surface </a> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHR_push_descriptor"> VK_KHR_push_descriptor </a> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHR_sampler_mirror_clamp_to_edge"> VK_KHR_sampler_mirror_clamp_to_edge </a> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHR_shader_draw_parameters"> VK_KHR_shader_draw_parameters </a> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHR_surface"> VK_KHR_surface </a> </li>
@@ -19,6 +21,7 @@
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_EXT_debug_marker"> VK_EXT_debug_marker </a> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_EXT_debug_report"> VK_EXT_debug_report </a> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_EXT_direct_mode_display"> VK_EXT_direct_mode_display </a> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_EXT_discard_rectangles"> VK_EXT_discard_rectangles </a> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_EXT_display_control"> VK_EXT_display_control </a> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_EXT_display_surface_counter"> VK_EXT_display_surface_counter </a> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_EXT_shader_subgroup_ballot"> VK_EXT_shader_subgroup_ballot </a> </li>
@@ -36,15 +39,37 @@
     <li> <b>  IMG Extensions (Imagination Technologies)  </b> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_IMG_filter_cubic"> VK_IMG_filter_cubic </a> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_IMG_format_pvrtc"> VK_IMG_format_pvrtc </a> </li>
+    <li> <b>  KHX Extensions (full vendor description unavailable)  </b> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHX_device_group"> VK_KHX_device_group </a> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHX_device_group_creation"> VK_KHX_device_group_creation </a> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHX_external_memory"> VK_KHX_external_memory </a> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHX_external_memory_capabilities"> VK_KHX_external_memory_capabilities </a> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHX_external_memory_fd"> VK_KHX_external_memory_fd </a> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHX_external_memory_win32"> VK_KHX_external_memory_win32 </a> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHX_external_semaphore"> VK_KHX_external_semaphore </a> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHX_external_semaphore_capabilities"> VK_KHX_external_semaphore_capabilities </a> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHX_external_semaphore_fd"> VK_KHX_external_semaphore_fd </a> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHX_external_semaphore_win32"> VK_KHX_external_semaphore_win32 </a> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHX_multiview"> VK_KHX_multiview </a> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_KHX_win32_keyed_mutex"> VK_KHX_win32_keyed_mutex </a> </li>
+    <li> <b>  MVK Extensions (The Brenwill Workshop Ltd.)  </b> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_MVK_ios_surface"> VK_MVK_ios_surface </a> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_MVK_macos_surface"> VK_MVK_macos_surface </a> </li>
     <li> <b>  NN Extensions (Nintendo Co., Ltd.)  </b> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_NN_vi_surface"> VK_NN_vi_surface </a> </li>
     <li> <b>  NV Extensions (NVIDIA Corporation)  </b> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_NV_clip_space_w_scaling"> VK_NV_clip_space_w_scaling </a> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_NV_dedicated_allocation"> VK_NV_dedicated_allocation </a> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_NV_external_memory"> VK_NV_external_memory </a> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_NV_external_memory_capabilities"> VK_NV_external_memory_capabilities </a> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_NV_external_memory_win32"> VK_NV_external_memory_win32 </a> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_NV_geometry_shader_passthrough"> VK_NV_geometry_shader_passthrough </a> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_NV_glsl_shader"> VK_NV_glsl_shader </a> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_NV_sample_mask_override_coverage"> VK_NV_sample_mask_override_coverage </a> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_NV_viewport_array2"> VK_NV_viewport_array2 </a> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_NV_viewport_swizzle"> VK_NV_viewport_swizzle </a> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_NV_win32_keyed_mutex"> VK_NV_win32_keyed_mutex </a> </li>
     <li> <b>  NVX Extensions (NVIDIA Corporation)  </b> </li>
     <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_NVX_device_generated_commands"> VK_NVX_device_generated_commands </a> </li>
+    <li> <a href="specs/1.0-extensions/html/vkspec.html#VK_NVX_multiview_per_view_attributes"> VK_NVX_multiview_per_view_attributes </a> </li>
 </ul>
